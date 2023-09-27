40070847dae770693608204e4f2b98ed79b2a068
dparol <dikshithparol123@gmail.com>
2023-09-27 23:51:42 +0530

    Preparing for Heroku deployment

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+# class PersonalityTypes(models.Model):
+#     Typename =models.CharField(max_length=100)
+#     TypeId = models.CharField(max_length=50)
+#     TypeDescription = models.
