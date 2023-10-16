ae0b189816dc2e1c6c568eb98fa8b8b2f5a92ab3
dparol <dikshithparol123@gmail.com>
2023-10-16 15:42:25 +0530

    'database'

--- Mindmatch/settings.py
+++ Mindmatch/settings.py
@@ -67,16 +67,20 @@ WSGI_APPLICATION = 'Mindmatch.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'Mindmatch',
+       'USER': 'postgres',
+       'PASSWORD': 'qwedfgbnm',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
 
 
-STATICFILES_DIRS = [
-     BASE_DIR / "Static_Files"
- ]
+# STATICFILES_DIRS = [
+#      BASE_DIR / "Static_Files"
+#  ]
 
 
 # Password validation
