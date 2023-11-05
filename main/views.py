ca0ebbfc498614980b7c0369c4dd7c26272189d0
dparol <dikshithparol123@gmail.com>
2023-11-05 11:14:47 +0530

    'findpersonality'

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import PersonalityTypes
 from django.shortcuts import render
 from .forms import PersonalityQtnForm
@@ -54,7 +54,7 @@ def personalityTest(request):
     ("Are you inclined to complete tasks before relaxing (J) or leave things open-ended and enjoy the process (P)? ", 'J', 'P'),
     ("Do you tend to be more methodical and systematic (J) or improvisational and opportunistic (P)? ", 'J', 'P'),
     ]
-    
+  
     if request.method == 'POST':
         form = PersonalityQtnForm(questions, request.POST)
         if form.is_valid():
@@ -64,11 +64,114 @@ def personalityTest(request):
             answers = [form.cleaned_data[f"question_{i}"] for i in range(len(questions))]
             # Further processing or saving the answers
             # Redirect or render a response
-            print(answers)
             
+            #find the number of yes or no
+            print(answers)
+             #find corresponding personality indicators
+            ans_list=map_answers_to_personality(answers)
+            print(ans_list) 
+            #get correct personality type
+            personality_type =   calculate_personality_type(ans_list)   
+            print(personality_type)         
 
     else:
         form = PersonalityQtnForm(questions)
 
     return render(request, 'adminPage.html', {'form': form, 'questions': questions})
 
+
+def map_answers_to_personality(answers):
+    personality_types = []
+    for i in range(0, len(answers),4):
+        # Mapping for I/E (example)
+        if answers[i] == 'yes':
+            personality_types.append('I')
+        else:
+            personality_types.append('E')
+
+        # Mapping for S/N (example)
+        if answers[i+1] == 'yes':
+            personality_types.append('S')
+        else:
+            personality_types.append('N')
+
+        # Mapping for T/F (example)
+        if answers[i+2] == 'yes':
+            personality_types.append('T')
+        else:
+            personality_types.append('F')
+
+        # Mapping for J/P (example)
+        if answers[i+3] == 'yes':
+            personality_types.append('J')
+        else:
+            personality_types.append('P')
+
+    return personality_types
+
+
+def calculate_personality_type(answers):
+            print(answers,"ddddddddddddddddddddddddddddddddd")
+            personality_type =''
+            # Calculate scores for each dimension\n",
+            I = answers.count('I')
+            E = answers.count('E')
+            S = answers.count('S')
+            N = answers.count('N')
+            T = answers.count('T')
+            F = answers.count('F')
+            J = answers.count('J')
+            P = answers.count('P')
+            print(I)
+            print(E)
+            print(S)
+            print(N)
+            print(T)
+            print(F)
+            print(J)
+            print(P)
+        
+            # Determine the first dimension (I/E)\n",
+            if I > E:
+                personality_type += 'I'
+            else:
+                personality_type += 'E'
+         # Determine the second dimension (S/N)\n",
+            if S > N:
+                personality_type += 'S'
+            else:
+                personality_type += 'N'
+        
+            # Determine the third dimension (T/F)\n",
+            if T > F:
+                personality_type += 'T'
+            else:
+                personality_type += 'F'
+  
+            # Determine the fourth dimension (J/P)\n",
+            if J > P:
+                personality_type += 'J'
+            else:
+                personality_type += 'P'
+    
+            # Define personality type based on the four dimensions\n",
+            personality_types = {
+                'ISTJ': 'ISTJ--The Inspector',
+                'ISFJ': 'ISFJ--The Protector',
+                'INFJ': 'INFJ--The Counselor',
+                'INTJ': 'INTJ--The Mastermind',
+                'ISTP': 'ISTP--The Craftsman',
+                'ISFP': 'ISFP--The Composer',
+                'INFP': 'INFP--The Healer',
+                'INTP': 'INTP--The Architect',
+                'ESTP': 'ESTP--The Dynamo',
+                'ESFP': 'ESFP--The Performer',
+                'ENFP': 'ENFP--The Champion',
+                'ENTP': 'ENTP--The Visionary',
+                'ESTJ': 'ESTJ--The Supervisor',
+                'ESFJ': 'ESFJ--The Provider',
+               'ENFJ': 'ENFJ--The Teacher',
+                'ENTJ': 'ENTJ--The Commander',
+            
+            }
+            return personality_types.get(personality_type)
